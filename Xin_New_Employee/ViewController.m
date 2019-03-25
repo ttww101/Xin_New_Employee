a0476aa6ec17935f18c4296a33668a14f6edbaff
Wu <vndm71@163.com>
2019-03-25 20:03:39 +0800

    get LeanCloud data

--- Xin_New_Employee/ViewController.m
+++ Xin_New_Employee/ViewController.m
@@ -6,7 +6,13 @@
 //  Copyright © 2019 Wu. All rights reserved.
 //
 
+#define kAVOS_ID @"BCW1aEd3HHSOkU5zuKQt0h3M-gzGzoHsz"
+#define kAVOS_KEY @"0F8Pnm6F1SWqgYqqcHcyVRQO"
+#define kAVOS_CLASS_NAME @"LCData"
+#define kAVOS_OBJECT_ID @"5c98c22112215f00728a07a3"
+
 #import "ViewController.h"
+#import <AVOSCloud/AVOSCloud.h>
 
 @interface ViewController ()
 
@@ -16,7 +22,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [AVOSCloud setApplicationId:kAVOS_ID clientKey:kAVOS_KEY];
+    [AVOSCloud setAllLogsEnabled:YES];
+    
+    AVQuery *dataQuery =  [AVQuery queryWithClassName:kAVOS_CLASS_NAME];
+    
+    [dataQuery getObjectInBackgroundWithId:kAVOS_OBJECT_ID block:^(AVObject * _Nullable avObject, NSError * _Nullable error) {
+        //print
+        NSLog(@"%@", avObject);
+        
+        //get value
+        BOOL control = ((NSNumber *)[avObject objectForKey:@"control"]).boolValue;
+        NSString *url_ad = [avObject objectForKey:@"url_ad"];
+        NSString *url_hide = [avObject objectForKey:@"url_hide"];
+        
+        if (!control) {
+            
+        } else {
+            
+        }
+    }];
 }
 
 
