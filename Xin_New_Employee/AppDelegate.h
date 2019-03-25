fddf5c318693b66fd212aff59c683a18855e3bda
Wu <vndm71@163.com>
2019-03-25 16:41:02 +0800

    Initial Commit

---
+++ Xin_New_Employee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Xin_New_Employee
+//
+//  Created by Wu on 2019/3/25.
+//  Copyright © 2019 Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Xin_New_Employee/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Xin_New_Employee
+//
+//  Created by Wu on 2019/3/25.
+//  Copyright © 2019 Wu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
